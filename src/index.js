80d636ec031d330508537f589e8bdf87ef06b729
manlili <manlili@lechebang.com>
2017-10-09 15:52:51 +0800

    add plugin template

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import VueLocalStorage from './VueLocalStorage'
+
+export default {
+  /**
+   * Install vue-local-storage plugin
+   *
+   * @param {Vue} Vue
+   * @param {Object} options
+   */
+  install (Vue, options = {}) {
+    if (typeof process !== 'undefined' &&
+      (
+        process.server ||
+        process.SERVER_BUILD ||
+        (process.env && process.env.VUE_ENV === 'server')
+      )
+    ) {
+      return
+    }
+
+    try {
+      const test = '__vue-localstorage-test__'
+
+      window.localStorage.setItem(test, test)
+      window.localStorage.removeItem(test)
+    } catch (e) {
+      console.error('Local storage is not supported')
+    }
+
+    const name = options.name || 'localStorage'
+
+    Vue.mixin({
+      created () {
+        if (this.$options[name]) {
+          Object.keys(this.$options[name]).forEach((key) => {
+            const [type, defaultValue] = [this.$options[name][key].type, this.$options[name][key].default]
+
+            VueLocalStorage.addProperty(key, type, defaultValue)
+          })
+        }
+      }
+    })
+
+    Vue[name] = VueLocalStorage
+    Vue.prototype[`$${name}`] = VueLocalStorage
+  }
+}
